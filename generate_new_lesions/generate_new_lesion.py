bf44229fb4bc55678cca00c83f6387814e6979a6
Naga Karthik <emvnagakarthik@gmail.com>
2023-04-24 22:59:42 -0400

    add lesions from pathology to healthy

--- generate_new_lesions/generate_new_lesion.py
+++ generate_new_lesions/generate_new_lesion.py
@@ -16,21 +16,26 @@ from tqdm import tqdm
 import argparse
 import os
 
+# TODO: Check out Diffusion models for synthesizing new images + lesions 
 
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("--generate_number", "-num", default=5, type=int,
                         help="Number of samples you want to crete: ")
-    parser.add_argument("--imagesTr_path", "-imgTr", default="imagesTr", type=str,
-                        help="Path to raw images")
-    parser.add_argument("--masksTr_path", "-mskTr", default="masksTr", type=str,
-                        help="Path to SC masks")
-    parser.add_argument("--labelsTr_path", "-labelTr", default="labelsTr", type=str,
-                        help="Path to lesion labels")
-    parser.add_argument("--mask_check_path", "-mask", default="mask", type=str,
-                        help="Path to save masks")
-    parser.add_argument("--mixid_csv_path", "-csv", default="mixid_csv.csv", type=str,
-                        help="Path to save csv file")
+    
+    parser.add_argument("--dir_pathology", "-dir-patho", default="imagesTr", type=str,
+                        help="Path to raw images from pathology dataset (i.e. SCI-Zurich)")
+    parser.add_argument("--dir_healthy", "-dir-health", default="imagesTr", type=str,
+                        help="Path to raw images from the healthy dataset (i.e. Spine Generic Multi)")
+    parser.add_argument("--dir_lesions", "-dir-lesion", default="labelsTr", type=str,
+                        help="Path to lesion labels from pathology dataset (i.e. SCI-Zurich)")
+    parser.add_argument("--dir_masks", "-dir-mask", default="masksTr", type=str,
+                        help="Path to SC masks from healthy dataset (i.e. Spine Generic Multi)")
+    parser.add_argument("--dir_save", "-dir-save", default="labelsTr", type=str,
+                        help="Path to save new lesion samples")
+    # parser.add_argument("--mask_save_path", "-mask-pth", default="mask", type=str,
+    #                     help="Path to save carved masks")
+
     return parser
 
 
@@ -42,6 +47,14 @@ def get_head(img_path):
 
     return spacing, direction, origin
 
+def set_orientation(img, spacing, direction, origin):
+    
+    # temp = sitk.ReadImage(img_path)
+    img.SetSpacing(spacing)
+    img.SetDirection(direction)
+    img.SetOrigin(origin)
+
+    return img
 
 def copy_head_and_right_xyz(data, spacing, direction, origin):
     TrainData_new = data.astype('float32')
@@ -67,94 +80,124 @@ def pad(image, pad_size):
     return image_padd
 
 
-def pad_or_crop(target_a, target_b, label_a, label_b):
+def pad_or_crop(img_healthy, mask_sc, img_patho, label_patho):
 
     # size to match after padding
-    if target_a.shape > target_b.shape:
-        spatial_size = target_a.shape
+    if img_healthy.shape > img_patho.shape:
+        spatial_size = img_healthy.shape
         # print("padding image_b to match the dimensions of image_a")
 
         # the pad function automatically pads the image to the largest dimensions 
         # between the two images. This might result in shape mismatch hence the next step
         # is to crop the padded image to match the size of the other image
-        target_b = pad(target_b, spatial_size)
-        label_b = pad(label_b, spatial_size)
+        img_patho = pad(img_patho, spatial_size)
+        label_patho = pad(label_patho, spatial_size)
         # print("paddded image_b: ", target_b.shape)
 
-        if target_b.shape != target_a.shape:
+        # TODO: check if this is necessary
+        if img_patho.shape != img_healthy.shape:
             # crop the padded image until it matches the size of the other image
-            if target_b.shape[0] > target_a.shape[0]:
-                target_b = target_b[0:target_a.shape[0], :, :]
-                label_b = label_b[0:label_a.shape[0], :, :]
+            if img_patho.shape[0] > img_healthy.shape[0]:
+                img_patho = img_patho[0:img_healthy.shape[0], :, :]
+                label_patho = label_patho[0:mask_sc.shape[0], :, :]
             
-            if target_b.shape[1] > target_a.shape[1]:
-                target_b = target_b[:, 0:target_a.shape[1], :]
-                label_b = label_b[:, 0:label_a.shape[1], :]
-            
-            if target_b.shape[2] > target_a.shape[2]:
-                target_b = target_b[:, :,  0:target_a.shape[2]]
-                label_b = label_b[:, :, 0:label_a.shape[2]]
+            if img_patho.shape[1] > img_healthy.shape[1]:
+                img_patho = img_patho[:, 0:img_healthy.shape[1], :]
+                label_patho = label_patho[:, 0:mask_sc.shape[1], :]
+
+            if img_patho.shape[2] > img_healthy.shape[2]:
+                img_patho = img_patho[:, :,  0:img_healthy.shape[2]]
+                label_patho = label_patho[:, :, 0:mask_sc.shape[2]]
             
             # print("padded and cropped image_b: ", target_b.shape)
 
     else:
-        spatial_size = target_b.shape
+        spatial_size = img_patho.shape
         # print("padding image_a to match the dimensions of image_b")
 
-        target_a = pad(target_a, spatial_size)
-        label_a = pad(label_a, spatial_size)
+        img_healthy = pad(img_healthy, spatial_size)
+        mask_sc = pad(mask_sc, spatial_size)
         # print("paddded image_a: ", target_a.shape)
 
-        if target_b.shape != target_a.shape:
+        # TODO: check if this is necessary
+        if img_patho.shape != img_healthy.shape:
             # crop the padded image until it matches the size of the other image
-            if target_a.shape[0] > target_b.shape[0]:
-                target_a = target_a[0:target_b.shape[0], :, :]
-                label_a = label_a[0:label_b.shape[0], :, :]
-            
-            if target_a.shape[1] > target_b.shape[1]:
-                target_a = target_a[:, 0:target_b.shape[1], :]
-                label_a = label_a[:, 0:label_b.shape[1], :]
-            
-            if target_a.shape[2] > target_b.shape[2]:
-                target_a = target_a[:, :,  0:target_b.shape[2]]
-                label_a = label_a[:, :, 0:label_b.shape[2]]
-            
+
+            if img_healthy.shape[0] > img_patho.shape[0]:
+                img_healthy = img_healthy[0:img_patho.shape[0], :, :]
+                mask_sc = mask_sc[0:label_patho.shape[0], :, :]
+
+            if img_healthy.shape[1] > img_patho.shape[1]:
+                img_healthy = img_healthy[:, 0:img_patho.shape[1], :]
+                mask_sc = mask_sc[:, 0:label_patho.shape[1], :]
+
+            if img_healthy.shape[2] > img_patho.shape[2]:
+                img_healthy = img_healthy[:, :,  0:img_patho.shape[2]]
+                mask_sc = mask_sc[:, :, 0:label_patho.shape[2]]
+
             # print("padded and cropped image_a: ", target_a.shape)
 
-    return target_a, target_b, label_a, label_b
+    # check whether the padded images, label and sc_mask have the same shape
+    assert img_healthy.shape == img_patho.shape, "image_a and image_b have different shapes"
+    assert mask_sc.shape == label_patho.shape, "mask_sc and label have different shapes"
+
+    return img_healthy, mask_sc, img_patho, label_patho
 
 
 
-def generate_new_sample(image_a, image_b, mask_a, mask_b, label_a, label_b):
-    spacing, direction, origin = get_head(image_a)
+def generate_new_sample(path_image_patho, path_image_healthy, path_mask_sc, path_label_patho):
 
-    image_a = nib.load(image_a).get_fdata()
-    image_b = nib.load(image_b).get_fdata()
-    mask_a = nib.load(mask_a).get_fdata()
-    mask_b = nib.load(mask_b).get_fdata()
-    label_a = nib.load(label_a).get_fdata()
-    label_b = nib.load(label_b).get_fdata()
+    # this is copying lesion from image_patho to image_healthy (and saving a new label (with lesion) in label_healthy)
+    # image_a = healthy image
+    # image_b = patho image
+    # label_a = sc_mask
+    # label_b = label_patho
 
-    # pad and/or crop images and labels so that they have the same shape
-    image_a, image_b, label_a, label_b = pad_or_crop(image_a, image_b, label_a, label_b)
+    # get the header of the healthy image
+    spacing, direction, origin = get_head(path_image_healthy)
+
+    # use the healthy header info to change the orientation of the patho image
+    # this changes the "world" orientation of XYZ
+    image_patho_temp = sitk.DICOMOrient(sitk.ReadImage(path_image_patho), "RAS")
+    label_patho_temp = sitk.DICOMOrient(sitk.ReadImage(path_label_patho), "RAS")
+    # this changes the "voxel" orientation of XYZ
+    image_patho_reoriented = set_orientation(image_patho_temp, spacing, direction, origin)
+    label_patho_reoriented = set_orientation(label_patho_temp, spacing, direction, origin)
+    
+    # return image_patho_reoriented, label_patho_reoriented
+ 
+    image_healthy = nib.load(path_image_healthy).get_fdata()
+    mask_sc = nib.load(path_mask_sc).get_fdata()
+    image_patho = sitk.GetArrayFromImage(image_patho_reoriented).transpose(2, 1, 0)
+    label_patho = sitk.GetArrayFromImage(label_patho_reoriented).transpose(2, 1, 0)
+
+    # # pad and/or crop images and labels so that they have the same shape
+    # image_healthy, mask_sc, image_patho, label_patho = pad_or_crop(image_healthy, mask_sc, 
+    #                                                                image_patho_reoriented, label_patho_reoriented)
+
+    # TODO: Need the SC mask for the pathlogy image to calculate the ratio of intensities of the 
+    # SC and the lesion. 
 
     # normalize images
-    image_a = (image_a - np.mean(image_a)) / np.std(image_a)
-    image_b = (image_b - np.mean(image_b)) / np.std(image_b)
+    image_healthy = (image_healthy - np.mean(image_healthy)) / np.std(image_healthy)
+    image_patho = (image_patho - np.mean(image_patho)) / np.std(image_patho)
 
     # Initialize new_target and new_label with the same shape as target_a
-    new_target = np.copy(image_a)
-    new_label = np.copy(label_a)
+    new_target = np.copy(image_healthy)
+    new_label = np.zeros_like(mask_sc)  # because we're creating a new label
 
-    # Create 3D bounding box around non-zero pixels in label_b
-    coords = np.argwhere(label_b > 0)
+    # Create 3D bounding box around non-zero pixels in label_patho
+    coords = np.argwhere(label_patho > 0)
     x0, y0, z0 = coords.min(axis=0)
     x1, y1, z1 = coords.max(axis=0) + 1  # slices are exclusive at the top
+    
+    # get the set of all coordinates where SC mask is 1
+    new_coords = np.argwhere(mask_sc > 0)
 
-    # Select random coordinate in new_target where mask_a is 1
-    # TODO - now, random coordinate may be selected close to the edge of the SC --> lesion may be projected outside of
-    #  the SC
-    new_coords = np.argwhere(mask_a > 0)
+    # make sure that the z-axis is at the max of the SC mask so that it is not mapped on the brainstem
+    new_coords = new_coords[new_coords[:, 2] < new_coords[:, 2].max()]
+
+    # Select random coordinate in new_target where SC mask is 1
     x, y, z = new_coords[random.randint(0, len(new_coords) - 1)]
 
     # Insert lesion from the bounding box to the new_target
@@ -163,9 +206,12 @@ def generate_new_sample(image_a, image_b, mask_a, mask_b, label_a, label_b):
             for z_step, z_cor in enumerate(range(z0, z1)):
                 # Insert only voxels corresponding to the lesion mask (label_b)
                 # Also make sure that the new lesion is not projected outside of the SC
-                if label_b[x_cor, y_cor, z_cor] > 0 and mask_a[x + x_step, y + y_step, z + z_step] > 0:
-                    new_target[x + x_step, y + y_step, z + z_step] = image_b[x_cor, y_cor, z_cor] * ratio
-                    new_label[x + x_step, y + y_step, z + z_step] = label_b[x_cor, y_cor, z_cor]
+                if label_patho[x_cor, y_cor, z_cor] > 0 and mask_sc[x + x_step, y + y_step, z + z_step] > 0:
+                    if x + x_step >= new_target.shape[0] or y + y_step >= new_target.shape[1] or z + z_step >= new_target.shape[2]:
+                        continue
+                    else:
+                        new_target[x + x_step, y + y_step, z + z_step] = image_patho[x_cor, y_cor, z_cor]
+                        new_label[x + x_step, y + y_step, z + z_step] = label_patho[x_cor, y_cor, z_cor]
 
     # Copy header information from target_a to new_target and new_label
     new_target = copy_head_and_right_xyz(new_target, spacing, direction, origin)
@@ -179,16 +225,27 @@ def main():
     parser = get_parser()
     args = parser.parse_args()
 
-    if not os.path.exists(args.mask_check_path):
-        os.makedirs(args.mask_check_path, exist_ok=True)
+    # if not os.path.exists(args.mask_check_path):
+    #     os.makedirs(args.mask_check_path, exist_ok=True)
 
-    Cases = os.listdir(args.labelsTr_path)
+    # get all pathology cases
+    cases_patho = os.listdir(args.dir_lesions)
     # remove '.DS_Store' from Cases list
-    if '.DS_Store' in Cases:
-        Cases.remove('.DS_Store')
-    simpleCases = [case.split('.')[0] for i, case in enumerate(Cases) if 'Mix' not in case]
-    Cases = simpleCases
-    prefix = Cases[0].split('_')[0]
+    if '.DS_Store' in cases_patho:
+        cases_patho.remove('.DS_Store')
+    simple_cases_patho = [case.split('.')[0] for i, case in enumerate(cases_patho) if 'Mix' not in case]
+    cases_patho = simple_cases_patho[1:]
+    prefix_patho = cases_patho[0].split('_')[0]
+
+    # get all healthy cases
+    cases_healthy = os.listdir(args.dir_masks)
+    # remove '.DS_Store' from Cases list
+    if '.DS_Store' in cases_healthy:
+        cases_healthy.remove('.DS_Store')
+    simple_cases_healthy = [case.split('.')[0] for i, case in enumerate(cases_healthy) if 'Mix' not in case]
+    cases_healthy = simple_cases_healthy[1:]
+    prefix_healthy = cases_healthy[0].split('_')[0]
+
 
     """
     Prepare data split, note that validation sets do not participate in 
@@ -196,49 +253,55 @@ def main():
     independently in nnunet.training.network_training.nnUNetTrainerV2.do_split 
     when using nnUNet framework
     """
-    num = len(Cases)
-    print('all_set_size: ', num)
-    Cases.sort()
-    start = 1
-    val_num = int(num * 0.2)
-    random.seed(985)
-    val_id = random.sample(range(1, num - 1), val_num)
-    val_id.sort()
-    val_set = [Cases[i - start] for i in val_id]
-    print('val_set:', val_set)
-    print('We use 20% num of imagesTr for validation,\n \
-        if you are using demo data file, it will be null')
-    tr_set = list(set(Cases) - set(val_set))
-    Cases = tr_set
-    print('====================================')
-    print('=Only select train_set for CarveMix=')
-    print('============val_set_not=============')
-    print('==========tr_set_size:%d============' % len(Cases))
-    print('====================================')
+    # num = len(Cases)
+    # print('all_set_size: ', num)
+    # Cases.sort()
+    # start = 1
+    # val_num = int(num * 0.2)
+    # random.seed(985)
+    # val_id = random.sample(range(1, num - 1), val_num)
+    # val_id.sort()
+    # val_set = [Cases[i - start] for i in val_id]
+    # print('val_set:', val_set)
+    # print('We use 20% num of imagesTr for validation,\n \
+    #     if you are using demo data file, it will be null')
+    # tr_set = list(set(Cases) - set(val_set))
+    # Cases = tr_set
+    # print('====================================')
+    # print('=Only select train_set for CarveMix=')
+    # print('============val_set_not=============')
+    # print('==========tr_set_size:%d============' % len(Cases))
+    # print('====================================')
     print(args)
-
-    with open(args.mixid_csv_path, 'w') as f:
-        f.write('id,mixid1,mixid2,lam\n')
 
     """
     Start generating new samples
     """
     for i in tqdm(range(args.generate_number), desc="mixing:"):
-        rand_index_a = random.randint(0, len(Cases) - 1)
-        rand_index_b = random.randint(0, len(Cases) - 1)
 
-        image_a = os.path.join(args.imagesTr_path, Cases[rand_index_a] + '_0000.nii.gz')
-        mask_a = os.path.join(args.masksTr_path, Cases[rand_index_a] + '_mask.nii.gz')
-        label_a = os.path.join(args.labelsTr_path, Cases[rand_index_a] + '.nii.gz')
-        image_b = os.path.join(args.imagesTr_path, Cases[rand_index_b] + '_0000.nii.gz')
-        mask_b = os.path.join(args.masksTr_path, Cases[rand_index_b] + '_mask.nii.gz')
-        label_b = os.path.join(args.labelsTr_path, Cases[rand_index_b] + '.nii.gz')
+        rand_index_patho = random.randint(0, len(cases_patho) - 1)
+        rand_index_healthy = random.randint(0, len(cases_healthy) - 1)
 
-        new_target, new_label = generate_new_sample(image_a, image_b, mask_a, mask_b, label_a, label_b)
+        print("Patho subject: ", cases_patho[rand_index_patho], '\t', "Healthy subject: ", cases_healthy[rand_index_healthy])
+
+        img_patho = os.path.join(args.dir_pathology, cases_patho[rand_index_patho] + '_0000.nii.gz')
+        lbl_patho = os.path.join(args.dir_lesions, cases_patho[rand_index_patho] + '.nii.gz')
+        
+        img_healthy = os.path.join(args.dir_healthy, cases_healthy[rand_index_healthy] + '_0000.nii.gz')
+        msk_sc_healthy = os.path.join(args.dir_masks, cases_healthy[rand_index_healthy] + '.nii.gz')
+        
+        new_target, new_label = generate_new_sample(path_image_patho=img_patho, path_label_patho=lbl_patho,
+                                         path_image_healthy=img_healthy, path_mask_sc=msk_sc_healthy,)
 
         s = str(i)
-        sitk.WriteImage(new_target, os.path.join(args.imagesTr_path, prefix + '_CarveMix_' + s + '_0000.nii.gz'))
-        sitk.WriteImage(new_label, os.path.join(args.labelsTr_path, prefix + '_CarveMix_' + s + '.nii.gz'))
+        sitk.WriteImage(new_target, os.path.join(args.dir_healthy, prefix_healthy + '_SimpleMix_' + s + '_0000.nii.gz'))
+        sitk.WriteImage(new_label, os.path.join(args.dir_save, prefix_healthy + '_SimpleMix_' + s + '.nii.gz'))
+
+        # reoriented_img, reoriented_lbl = generate_new_sample(path_image_patho=img_patho, path_label_patho=lbl_patho,
+        #                                  path_image_healthy=img_healthy, path_mask_sc=msk_sc_healthy,)
+        # sitk.WriteImage(reoriented_img, os.path.join(args.dir_save, prefix_healthy + '_SimpleMix_img.nii.gz'))
+        # sitk.WriteImage(reoriented_lbl, os.path.join(args.dir_save, prefix_healthy + '_SimpleMix_lbl.nii.gz'))
+
 
 
 if __name__ == '__main__':
