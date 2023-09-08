a80fc1701dea46f0ad65f8c0cc48c364e032dad7
Naga Karthik <emvnagakarthik@gmail.com>
2023-09-08 10:46:17 -0400

    add args for tile step size

--- packaging/run_inference_single_subject.py
+++ packaging/run_inference_single_subject.py
@@ -13,6 +13,7 @@ Example spinal cord segmentation:
         -o sub-001_T2w_seg_nnunet.nii.gz
         -path-model /path/to/model
         -pred-type sc
+        -tile-step-size 0.5
 
 Example lesion segmentation:
     python run_inference_single_subject.py
@@ -20,6 +21,7 @@ Example lesion segmentation:
         -o sub-001_T2w_lesion_seg_nnunet.nii.gz
         -path-model /path/to/model
         -pred-type lesion
+        -tile-step-size 0.5
 """
 
 
@@ -53,7 +55,10 @@ def get_parser():
     parser.add_argument('-use-best-checkpoint', action='store_true', default=False,
                         help='Use the best checkpoint (instead of the final checkpoint) for prediction. '
                              'NOTE: nnUNet by default uses the final checkpoint. Default: False')
-
+    parser.add_argument('-tile-step-size', default=0.5, type=float,
+                        help='Tile step size defining the overlap between images patches during inference. Default: 0.5 '
+                                'NOTE: changing it from 0.5 to 0.9 makes inference faster but there is a small drop in '
+                                'performance.')
     return parser
 
 
@@ -158,9 +163,9 @@ def main():
         output_folder=tmpdir_nnunet,
         model_training_output_dir=args.path_model,
         use_folds=folds_avail,
-        tile_step_size=0.9,     # changing it from 0.5 to 0.9 makes inference faster
-        use_gaussian=True,      # applies gaussian noise and gaussian blur
-        use_mirroring=False,    # test time augmentation by mirroring on all axes
+        tile_step_size=args.tile_step_size,     # changing it from 0.5 to 0.9 makes inference faster
+        use_gaussian=True,                      # applies gaussian noise and gaussian blur
+        use_mirroring=False,                    # test time augmentation by mirroring on all axes
         perform_everything_on_gpu=True if args.use_gpu else False,
         device=torch.device('cuda', 0) if args.use_gpu else torch.device('cpu'),
         verbose=False,
