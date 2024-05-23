9e2ef3e4d9b725701be32e3a12e1dc5de3d6c8c8
Naga Karthik <emvnagakarthik@gmail.com>
2024-05-23 17:44:38 -0400

    add function for creating multichannel label input

--- dataset-conversion/utils.py
+++ dataset-conversion/utils.py
@@ -56,6 +56,41 @@ def create_region_based_label(lesion_label_file, seg_label_file, image_file, sub
     return label_nii
 
 
+def create_multi_channel_label_input(lesion_label_file, seg_label_file, image_file, sub_ses_name, thr=0.5):
+    """
+    Ensures lesion seg is part of SC seg (which will be the 2nd channel) in multi-channel input for nnUNet training. 
+    The label has 1 single channel --> 0: background, 1: lesion seg
+    The input has 2 channels --> image and spinal cord seg
+    """
+    # load the labels
+    lesion_label_npy = nib.load(lesion_label_file).get_fdata()
+    seg_label_npy = nib.load(seg_label_file).get_fdata()
+
+    # binarize the labels
+    lesion_label_npy = np.where(lesion_label_npy > thr, 1, 0)
+    seg_label_npy = np.where(seg_label_npy > thr, 1, 0)
+
+    # check if the shapes of the labels match
+    assert lesion_label_npy.shape == seg_label_npy.shape, \
+        f'Shape mismatch between lesion label and segmentation label for subject {sub_ses_name}. Check the labels.'
+
+    # create a new label array with the same shape as the original labels
+    label_npy = np.zeros(lesion_label_npy.shape, dtype=np.int16)
+    # spinal cord
+    label_npy[seg_label_npy == 1] = 1
+    # lesion seg
+    label_npy[lesion_label_npy == 1] = 1
+
+    # print unique values in the label array
+    # print(f'Unique values in the label array for subject {sub_ses_name}: {np.unique(label_npy)}')
+
+    # save the new label file
+    ref = nib.load(image_file)
+    label_nii = nib.Nifti1Image(label_npy, ref.affine, ref.header)
+
+    return label_nii
+
+
 def get_git_branch_and_commit(dataset_path=None):
     """
     :return: git branch and commit ID, with trailing '*' if modified
