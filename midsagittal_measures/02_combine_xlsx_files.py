d36ec226a7b1a3fcbdff2b67b283c0a1d7746c9c
valosekj <jan.valosek@upol.cz>
2025-07-30 12:38:56 +0200

    rename `02_read_xlsx_files.py` --> `02_combine_xlsx_files.py`

--- midsagittal_measures/02_combine_xlsx_files.py
+++ midsagittal_measures/02_combine_xlsx_files.py
@@ -9,10 +9,10 @@ Namely, the script:
  - aggregates the metrics across subjects into a single dataframe and save the dataframe to a CSV file
 
 Example usage:
-    python 02_read_xlsx_files.py -dir <DIR_NAME>/results -branch master -pred-type GT
-    python 02_read_xlsx_files.py -dir <DIR_NAME>/results -branch master -pred-type SCIsegV2
-    python 02_read_xlsx_files.py -dir <DIR_NAME>/results -branch PR4656 -pred-type GT
-    python 02_read_xlsx_files.py -dir <DIR_NAME>/results -branch PR4656 -pred-type SCIsegV2
+    python 02_combine_xlsx_files.py -dir <DIR_NAME>/results -branch master -pred-type GT
+    python 02_combine_xlsx_files.py -dir <DIR_NAME>/results -branch master -pred-type SCIsegV2
+    python 02_combine_xlsx_files.py -dir <DIR_NAME>/results -branch PR4656 -pred-type GT
+    python 02_combine_xlsx_files.py -dir <DIR_NAME>/results -branch PR4656 -pred-type SCIsegV2
 
 Note: to read XLSX files, you might need to install the following packages:
     pip install openpyxl
