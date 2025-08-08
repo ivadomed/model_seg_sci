fafab866c12b8e25a5abf0ba41ce95ef66e8dcbf
valosekj <jan.valosek@upol.cz>
2025-08-08 12:18:37 +0200

    tweak sct_qc -qc-subject

--- midsagittal_measures/01_compute_midsagittal_lesion_measures.sh
+++ midsagittal_measures/01_compute_midsagittal_lesion_measures.sh
@@ -156,7 +156,7 @@ mv ${file_t2}_sc_seg.nii.gz ${file_t2}_sc_seg_SCIsegV2.nii.gz
 
 # Generate sagittal lesion QC report (because sct_deepseg produces only axial QC report showing both SC and lesion).
 # But we want to show only the lesion segmentation in the QC report on sagittal slices.
-sct_qc -i ${file_t2}.nii.gz -d ${file_t2}_lesion_seg.nii.gz -s ${file_t2}_sc_seg_SCIsegV2.nii.gz -p sct_deepseg_lesion -plane sagittal -qc ${PATH_QC} -qc-subject ${SUBJECT}
+sct_qc -i ${file_t2}.nii.gz -d ${file_t2}_lesion_seg.nii.gz -s ${file_t2}_sc_seg_SCIsegV2.nii.gz -p sct_deepseg_lesion -plane sagittal -qc ${PATH_QC} -qc-subject "lesion_SCIsegV2"
 
 # Compute the midsagittal lesion length and width based on the spinal cord and lesion segmentations obtained using SCIsegV2
 status=0
