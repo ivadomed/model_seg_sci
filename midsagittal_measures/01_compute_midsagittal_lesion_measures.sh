b88b50bd38b51fb0d2c3ce1c9c1033f4bdc1a843
valosekj <jan.valosek@upol.cz>
2025-08-08 12:31:26 +0200

    improve logging

--- midsagittal_measures/01_compute_midsagittal_lesion_measures.sh
+++ midsagittal_measures/01_compute_midsagittal_lesion_measures.sh
@@ -127,7 +127,7 @@ status=0
 sct_analyze_lesion -m ${file_t2}_lesion-manual_bin.nii.gz -s ${file_t2}_seg-manual.nii.gz -qc ${PATH_QC} -qc-subject ${SUBJECT} || status=$?
 # If status is not zero, sct_analyze_lesion failed (e.g., because there is no lesion in the GT segmentation)
 if [ $status -ne 0 ]; then
-    echo "❌No lesion found in manual GT segmentation for subject ${file_t2}" >> ${PATH_LOG}/manual_GT_analysis.log
+    echo "❌No lesion found in manual GT segmentation for ${file_t2}" >> ${PATH_LOG}/manual_GT_analysis.log
     exit 0
 else
   # If sct_analyze_lesion finished successfully, the outputs are:
@@ -163,7 +163,7 @@ status=0
 sct_analyze_lesion -m ${file_t2}_lesion_seg.nii.gz -s ${file_t2}_sc_seg_SCIsegV2.nii.gz -qc ${PATH_QC} -qc-subject ${SUBJECT} || status=$?
 # If status is not zero, sct_analyze_lesion failed (e.g., because there is no lesion in the GT segmentation)
 if [ $status -ne 0 ]; then
-    echo "❌No lesion found in manual GT segmentation for subject ${file_t2}" >> ${PATH_LOG}/SCIsegV2_predictions_analysis.log
+    echo "❌No lesion segmented by SCIsegV2 for ${file_t2}" >> ${PATH_LOG}/SCIsegV2_predictions_analysis.log
     exit 0
 else
   # The outputs are:
@@ -179,7 +179,7 @@ else
   mv ${file_t2}_lesion_seg_analysis.xlsx ${file_t2}_lesion_seg_analysis_SCIsegV2.xlsx
   # Copy the XLSX file to the results folder
   cp ${file_t2}_lesion_seg_analysis_SCIsegV2.xlsx ${PATH_RESULTS}
-  echo "${file_t2}_lesion_seg_analysis_SCIsegV2.xlsx created" >> ${PATH_LOG}/SCIsegV2_predictions_analysis.log
+  echo "✅ ${file_t2}_lesion_seg_analysis_SCIsegV2.xlsx created" >> ${PATH_LOG}/SCIsegV2_predictions_analysis.log
 fi
 # ------------------------------------------------------------------------------
 # End
