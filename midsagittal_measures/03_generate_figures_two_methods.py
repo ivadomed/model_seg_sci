53adc2a774bb3bccdd5a6a8feae99a7be4d35421
valosekj <jan.valosek@upol.cz>
2025-08-08 14:11:52 +0200

    add legend title

--- midsagittal_measures/03_generate_figures_two_methods.py
+++ midsagittal_measures/03_generate_figures_two_methods.py
@@ -603,7 +603,8 @@ def create_clinical_metrics_plots(df_ses_01, output_dir):
             # Add legend
             handles, labels = ax.get_legend_handles_labels()
             by_label = dict(zip(labels, handles))
-            ax.legend(by_label.values(), by_label.keys(), loc='lower right', fontsize=FONT_SIZE-2, framealpha=0.9)
+            ax.legend(by_label.values(), by_label.keys(), loc='lower right', fontsize=FONT_SIZE-2, framealpha=0.9,
+                      title=f'{METRIC_TO_TITLE[metric].split("[")[0]}', title_fontsize=FONT_SIZE-2)
 
             # Remove the top and right spines
             ax.spines['top'].set_visible(False)
